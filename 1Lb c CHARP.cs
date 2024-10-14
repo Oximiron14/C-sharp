d0ae16b4542f7449bac9b70741c3aa8812bf638e
Oximiron14 <oximiron149@gmail.com>
2024-10-14 10:36:13 +0300

    Add files via upload

---
+++ 1Lb c CHARP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+       
+            double y = Convert.ToDouble(Console.ReadLine());
+          
+            double c = Math.Pow((y * Math.Pow(x, 2) - 1), 2) / 2 * (Math.Pow(Math.Cos(y), 2) - Math.Sin(Math.Pow(x, 2)));
+            Console.WriteLine(c);
+
+
+
+        }
+    }
+}
