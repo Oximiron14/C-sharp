497d1c18f7b7d59b39dc89c130cef399dc0e94ea
Oximiron14 <oximiron149@gmail.com>
2024-10-21 11:15:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _3LB_Ccharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double y;
+			double x;
+			double c = 7;
+
+			
+			for (x = 0; x <= 1; x += 0.1)
+			{
+				y = Math.Pow(2, -x) * Math.Pow(c, x);
+				Console.WriteLine(y);
+			}
+			Console.WriteLine(".......");
+
+			x = -1;
+
+
+
+			while (x <= 1)
+			{
+				y = Math.Pow(2, -x) * Math.Pow(c, x);
+				x += 0.1;
+				Console.WriteLine(y);
+			}
+			
+
+			x = 0.1;
+
+			do
+			{
+				y = Math.Pow(2, -x) * Math.Pow(c, x);
+				x += 0.1;
+				Console.WriteLine(y);
+			} while (x <= 1);
+
+
+		}
+	}
+    
+}
