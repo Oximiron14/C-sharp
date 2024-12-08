8e248219aa3b8c761f92dd49bc320bf28a78fba9
Oximiron14 <oximiron149@gmail.com>
2024-12-08 23:08:20 +0300

    ///
    ///

---
+++ 1Lb c CHARP.cs
@@ -0,0 +1,23 @@
+﻿//1Лб//
+//4ВАРИАНТ//
+
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+       
+            double y = Convert.ToDouble(Console.ReadLine());
+          
+            double c = Math.Pow((y * Math.Pow(x, 2) - 1), 2) / 2 * (Math.Pow(Math.Cos(y), 2) - Math.Sin(Math.Pow(x, 2)));
+            Console.WriteLine(c);
+
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,81 +1,49 @@
-﻿using System;
+﻿//3ЛБ//
+//ВАРИАНТ 3//
 
-namespace C_charp_LB2
+using System;
+
+namespace _3LB_Ccharp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double y;
+			double x;
+			double c = 7;
 
-            double x = Convert.ToDouble(Console.ReadLine());
 
-            double y = Convert.ToDouble(Console.ReadLine());
+			for (x = 0; x <= 1; x += 0.1)
+			{
+				y = Math.Pow(2, -x) * Math.Pow(c, x);
+				Console.WriteLine(y);
+			}
+			Console.WriteLine(".......");
 
-            double e = Convert.ToDouble(Console.ReadLine());
-
-            double a = Convert.ToDouble(Console.ReadLine());
-
-            double b = Convert.ToDouble(Console.ReadLine());
-
-            double n = (0);
-
-            double g = Convert.ToDouble(Console.ReadLine());
-
-            double l = Convert.ToDouble(Console.ReadLine());
-
-            double minEl = y;
-            if (minEl > a * x)
-            {
-                minEl = a * x;
-            }
-
-            double maxEl = Math.Pow(x, 3) * Math.Pow(e, y);
-            if (maxEl > Math.Sqrt(Math.Log(Math.Pow(y, 2))))
-            {
-                maxEl = Math.Sqrt(Math.Log(Math.Pow(y, 2)));
-            }
-
-            double k;
-
-            if (x * y > 0)
-            {
-
-                double z = Math.Pow(1 - e, x * y + a * b);
-                Console.WriteLine(z);
-
-                if (minEl > a * x) ;
-                {
-                    minEl = a * x;
-
-                }
-                double p = maxEl;
-                p = Math.Pow(x, 3) * Math.Pow(e, y);
-
-                if (p > Math.Sqrt(Math.Log(y, 2))) ;
-
-                maxEl = Math.Sqrt(Math.Log(Math.Pow(y, 2)));
-
-                double q;
+			x = -1;
 
 
 
+			while (x <= 1)
+			{
+				y = Math.Pow(2, -x) * Math.Pow(c, x);
+				x += 0.1;
+				Console.WriteLine(y);
+			}
 
-            }
-            else if  (x * y == n)
-            {
-                double c = (b - (minEl));
 
-                Console.WriteLine(c);
-            }
-            else if (x * y < 0)
-            {
-                double j;
-                j = maxEl;
-                Console.WriteLine(j);
-            }
+			x = 0.1;
 
-        }
-    }
+			do
+			{
+				y = Math.Pow(2, -x) * Math.Pow(c, x);
+				x += 0.1;
+				Console.WriteLine(y);
+			} while (x <= 1);
+
+
+		}
+	}
+
 }
-
-            
